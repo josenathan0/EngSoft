a576d57c894d7c8768c3e0a4a380562b4d6bdace
jose.miguel <josenathan900@gmail.com>
2024-04-24 09:28:52 -0300

    up

--- src/project/Nathan.java
+++ src/project/Nathan.java
@@ -1,19 +1,23 @@
 package project;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Nathan {
+import java.util.Scanner;
+
+public class Nathan{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Digite sua primeira nota: ");
+        double nota1 = sc.nextDouble();
+        System.out.println("Digite sua segunda nota: ");
+        double nota2 = sc.nextDouble();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        double mediaSimples = (nota1 + nota2) / 2;
+
+        if (mediaSimples >= 7) {
+            System.out.println("aprovado");
+        }
+        else {
+            System.out.println("reprovado");
         }
     }
 }
